98af14f9932682eaf173c509a1e99a99b7e09681
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2021-03-04 21:46:44 +0600

    Create 2n_Grid_2_1_and_2_2_Sized_Tiles_No_Rotaion.cpp
    Given a 2*N Grid, how many ways we can cover the whole grid with tiles?

---
+++ 2n_Grid_2_1_and_2_2_Sized_Tiles_No_Rotaion.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastread     ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fileRead freopen("input.txt","r",stdin);
+#define fileWrite freopen("output.txt","w",stdout);
+#define ll long long
+#define mx 100009
+int dp[mx];
+int foo(int n)
+{
+    if(dp[n]!=-1)
+        return dp[n];
+    return dp[n]=foo(n-1)+foo(n-2);
+}
+int main()
+{
+    memset(dp,-1,sizeof dp);
+    dp[1]=1;
+    dp[2]=2;
+    int sizeOfTiles;
+    cin>>sizeOfTiles;
+    cout<<foo(sizeOfTiles)<<endl;;
+}
