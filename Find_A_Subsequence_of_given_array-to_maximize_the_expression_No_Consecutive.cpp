a27374650201bb4a1a198c8d8a19a08fa6701e1b
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2021-03-05 16:28:58 +0600

    Create Find_A_Subsequence_of_given_array-to_maximize_the_expression_No_Consecutive.cpp
    Given an array with N (N<100000) integers. Find a Subsequence not taking consecutive elements of this array to maximize the following expression.
    Expression:
    Ab1-Ab2+Ab3-Ab4+ ....

---
+++ Find_A_Subsequence_of_given_array-to_maximize_the_expression_No_Consecutive.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fastread     ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define fileRead freopen("input.txt","r",stdin);
+#define fileWrite freopen("output.txt","w",stdout);
+#define ll long long
+#define mx 100009
+int dp[mx][2];
+int n;
+int ara[mx];
+int foo(int pos,bool flag)
+{
+    if(pos>=n)
+        return 0;
+    if(dp[pos][flag]!=-1)
+        return dp[pos][flag];
+    if(flag==0)
+    {
+        dp[pos][flag]=max(ara[pos]+foo(pos+2,!flag),foo(pos+1,flag));
+    }
+    else
+    {
+        dp[pos][flag]=max(-ara[pos]+foo(pos+2,!flag),foo(pos+1,flag));
+    }
+    return dp[pos][flag];
+}
+int main()
+{
+    memset(dp,-1,sizeof dp);
+    cin>>n;
+    for(int i=0;i<n;i++)
+        cin>>ara[i];
+    cout<<foo(0,1)<<endl;;
+}
